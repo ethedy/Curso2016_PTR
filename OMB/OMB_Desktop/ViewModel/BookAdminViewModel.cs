fc30e8718bb78217ce6d5f715ce42e54d3156784
ethedy <ethedy@gmail.com>
2016-12-05 17:12:08 -0300

    Clase 16

--- OMB/OMB_Desktop/ViewModel/BookAdminViewModel.cs
+++ OMB/OMB_Desktop/ViewModel/BookAdminViewModel.cs
@@ -5,15 +5,47 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
+using Entidades;
+using Servicios;
 
 namespace OMB_Desktop.ViewModels
 {
   public class BookAdminViewModel : ViewModelBase
   {
+    #region Binding Properties
+
     private string _isbn13;
+
+    public string Isbn13
+    {
+      get { return _isbn13; }
+      set { Set(() => Isbn13, ref _isbn13, value); }
+    }
+
     private string _isbn10;
+
+    public string Isbn10
+    {
+      get { return _isbn10; }
+      set { Set(() => Isbn10, ref _isbn10, value); }
+    }
+
     private string _titulo;
+
+    public string Titulo
+    {
+      get { return _titulo; }
+      set { Set(() => Titulo, ref _titulo, value); }
+    }
+
     private string _subtitulo;
+
+    public string Subtitulo
+    {
+      get { return _subtitulo; }
+      set { Set(() => Subtitulo, ref _subtitulo, value); }
+    }
 
     private DateTime _fechaPublicacion;
 
@@ -23,13 +55,60 @@ namespace OMB_Desktop.ViewModels
       set { Set(() => FechaPublicacion, ref _fechaPublicacion, value); }
     }
 
+    private string _imagePath;
+
+    public string FilePathImage
+    {
+      get { return _imagePath; }
+      set { Set(() => FilePathImage, ref _imagePath, value); }
+    }
+
+    private List<Editorial> _editoriales;
+
+    public List<Editorial> Editoriales 
+    {
+      get { return _editoriales; }
+      set { Set(() => Editoriales, ref _editoriales, value); }
+    }
+
+    private Editorial _editorialSeleccionada;
+
+    public Editorial EditorialSeleccionada
+    {
+      get { return _editorialSeleccionada; }
+      set { Set(() => EditorialSeleccionada, ref _editorialSeleccionada, value); }
+    }
+
+    #endregion
+
+    #region Binding Commands
+
+    public ICommand GetImage { get; set; }
+
+    #endregion
+
     public BookAdminViewModel()
     {
       if (IsInDesignModeStatic)
       {
         FechaPublicacion = DateTime.Now;
+        FilePathImage = "Ingresar una ruta valida...";
       }
-      FechaPublicacion = DateTime.Now;
+      else
+      {
+        ProductServices prod = new ProductServices();
+
+        FechaPublicacion = DateTime.Now;
+
+        Editoriales = new List<Editorial>(prod.GetEditoriales());
+
+        GetImage = new RelayCommand(DoGetImage);
+      }
+    }
+
+    private void DoGetImage()
+    {
+      string file = _imagePath;
     }
   }
 }
